5d9bcbb0c360dc946857192dc9d0b2841ad12e12
Adro <acousens@gmail.com>
2020-06-11 16:00:15 -0700

    Add cors. Save new messages to db. Add route for fetching all messages

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 const express = require('express')
 const app = express();
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 const pusher = require('./pusher')
 
+const production = process.NODE_ENV === 'production';
+const origin = production ? process.env.origin : '*'
+
+db.defaults({ messages: []}).write()
+
+function cors(req, res) {
+  
+  res.header('Content-Type','application/json');
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method == 'OPTIONS') {
+    res.status(204).send('');
+  }
+
+  if (req.get('origin') === origin || !production) {
+    return 
+  }
+
+  res.status(401)
+}
+
+// TODO authorization
+app.get('/messages', (req, res) => {
+  
+  cors(req, res)
+  
+  res.json(db.get('messages'))
+})
+
 app.get('/', (req, res) => {
+  
+  cors(req, res)
+
+  var body = `Hello World ${Date.now()}`
+
+  db.get('messages')
+    .push({ body: body})
+    .write()  
   pusher.trigger('my-channel', 'my-event', {
-    'message': `Hello World ${Date.now()}`
+    body: body
   });
   res.status(200).send('')
 })
