95db4774ffca458749e8d1f846d4ab9d43c963c1
Adro <acousens@gmail.com>
2020-06-21 14:17:48 -0700

    Show env on status screen

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', (req, res) => {
-  res.status(200).send('OK')
+  res.status(200).send(`OK [${process.NODE_ENV}]`)
 })
 
 // TODO authorization
